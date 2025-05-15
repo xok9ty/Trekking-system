268bd5028adf3a299a3fd56e4866599464defb96
Кирилл <125658205+xok9ty@users.noreply.github.com>
2025-05-16 01:09:06 +0300

    create TaskDetail views, html, urls and update task_list.html

--- trekking/urls.py
+++ trekking/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskDetailView
 
-
+app_name = 'tasks'
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task-list'),
+    path('<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
 ]
--- trekking/views.py
+++ trekking/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Task
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class TaskListView(ListView):
     model = Task
     template_name = 'task_list.html'
     context_object_name = 'tasks'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
+    
